5d4cf4b25def2a5941680894efbc30eb4b97438c
xyleneSVG <dhavinf.a@gmail.com>
2026-01-14 18:23:18 +0700

    feat: suspense

--- src/components/DynamicRenderThemes.tsx
+++ src/components/DynamicRenderThemes.tsx
@@ -1,11 +1,24 @@
 "use client";
 
-import VintageJawaPage from "@/themes/dark/page";
 import { useSearchParams } from "next/navigation";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+const VintageJawaPage = dynamic(() => import("@/themes/dark/page"), {
+  ssr: false,
+  loading: () => <div className="min-h-screen w-full bg-white"></div>,
+});
 
-export default function DynamicRenderThemes() {
+function ThemeContent() {
   const searchParams = useSearchParams();
   const theme = searchParams.get("theme");
 
-  return <>{theme == "dark" && <VintageJawaPage />}</>;
+  return <>{theme === "dark" && <VintageJawaPage />}</>;
+}
+
+export default function DynamicRenderThemes() {
+  return (
+    <Suspense fallback={<div className="min-h-screen bg-white"></div>}>
+      <ThemeContent />
+    </Suspense>
+  );
 }
